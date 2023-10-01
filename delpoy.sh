20e79ef57fb9abef8595afb33604a4c494e2b059
Pestilent <pestilent@amaterasu>
2023-10-01 19:07:08 -0400

    breaking deploy.sh into multiple parts

--- delpoy.sh
+++ delpoy.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-#vars
-if [$(uname -o) = 'Darwin']; then
-     aarch="$(uname -m)-Darwin"
-else
-    aarch="$(uname -m)-linux"
-fi
-uname=$(whoami)
-sysname=$(hostname -s)
+# Download repo
+mkdir -p ~/src
+curl https://github.com/verchalent/macos-nix-basic/archive/refs/heads/master.zip
+cd src
+unzip ~/Downloads/macos-nix-basic-master.zip
 
 #Install nix
 sh <(curl -L https://nixos.org/nix/install)
@@ -15,21 +12,4 @@ sh <(curl -L https://nixos.org/nix/install)
 #Install Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-mkdir -p ~/src
-cd ~/src
-git clone https://github.com/verchalent/macos-nix-basic.git
-cd macos-nix-basic
-
-# Replace System Name, Username and Arch with correct values for system
-sed -i "s/SYSTEMNAME/$sysname/g" flake.nix
-sed -i "s/USERNAME/$uname/g" flake.nix
-sed -i "s/ARCH/$aarch/g" flake.nix
-
-# Add changed items back to local git to allow build
-git add .
-
-# Bootstrap Nix Darwin
-nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer ./result/bin/darwin-installer
-
-# Init Flake
-nix  --extra-experimental-features "nix-command flakes" run nix-darwin -- switch --flake ~/src/macos-nix-basic
+echo "Close this terminal and launch deploy2.sh"
---
+++ deploy2.sh
@@ -0,0 +1,26 @@
+
+#!/usr/bin/env bash
+
+#vars
+if [$(uname -o) = 'Darwin']; then
+     aarch="$(uname -m)-Darwin"
+else
+    aarch="$(uname -m)-linux"
+fi
+uname=$(whoami)
+sysname=$(hostname -s)
+
+# Replace System Name, Username and Arch with correct values for system
+sed -i "s/SYSTEMNAME/$sysname/g" flake.nix
+sed -i "s/USERNAME/$uname/g" flake.nix
+sed -i "s/ARCH/$aarch/g" flake.nix
+
+# Add changed items back to local git to allow build
+git add .
+
+# Below this line may need to be in new terminal
+# Bootstrap Nix Darwin
+nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer ./result/bin/darwin-installer
+
+# Init Flake
+nix  --extra-experimental-features "nix-command flakes" run nix-darwin -- switch --flake ~/src/macos-nix-basic
